c07b96d9205156c47cf7e58e442f675214cd2c1a
LENOVO <ecorewick@gmail.com>
2025-11-22 23:49:06 +0530

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app= express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req,res) => {
-    res.send("Hello get data");
+    res.send("Hello get data..................");
 });
+
+app.get("/a1", (req,res) => {
+    res.send("get data..................");
+});
+
 
 app.listen(port, () => {
     console.log("Server started on port 8080");
